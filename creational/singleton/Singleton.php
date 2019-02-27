53e69ed4904dfc2e6e2daa96d6bbf83e43fdee15
Favian-Ioel Poputa <favian.poputa@softvision.ro>
2019-02-27 19:56:22 +0200

    Update Singleton.php
    with phpdocs

--- creational/singleton/Singleton.php
+++ creational/singleton/Singleton.php
@@ -4,6 +4,11 @@ final class Singleton
 {
 	private static $instance;
 
+	/**
+	 * gets the instance via lazy initialization (created on first usage)
+	 *
+	 * @return Training
+	 */
 	public static function getInstance(): Singleton
 	{
 		if (null === static::$instance) {
@@ -12,14 +17,30 @@ final class Singleton
 		return static::$instance;
 	}
 
+	/**
+	 * is not allowed to call from outside to prevent from creating multiple instances,
+     	 * to use the singleton, you have to obtain the instance from Singleton::getInstance() instead
+	 *
+	 * @return void
+	 */
 	private function __construct()
 	{
 	}
-
+	
+	/**
+	 * prevent the instance from being cloned (which would create a second instance of it)
+	 *
+	 * @return void
+	 */
 	private function __clone()
 	{
 	}
 
+	/**
+	 * prevent from being unserialized (which would create a second instance of it)
+	 *
+	 * @return void
+	 */
 	private function __wakeup()
 	{
 	}
